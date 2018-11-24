b94e16ee8e734abd6cf747b195d6481ed8fd4405
jcroc32 <33503909+jcroc32@users.noreply.github.com>
2018-11-24 10:52:01 -0600

    Add files via upload
    trying to implement wavenet architecture

--- HRA_cnn_model.py
+++ HRA_cnn_model.py
@@ -34,27 +34,43 @@ x_test = np.dstack(x_test)
 x_train = np.dstack(x_train)
 y_test = to_categorical(y_test-1)
 y_train = to_categorical(y_train-1)
-# get data dimensions
-samples = x_train.shape[1]
-features = x_train.shape[2]
-outputs = y_train.shape[1]
-# create cnn model
-model = Sequential()
-model.add(Conv1D(filters=64, kernel_size=3, activation='relu', dilation_rate=2, input_shape=(samples,features)))
-model.add(Dropout(0.5))
-model.add(MaxPooling1D(pool_size=2))
-model.add(Conv1D(filters=32, kernel_size=3, activation='relu', dilation_rate=2))
-model.add(Flatten())
-model.add(Dense(100, activation='relu'))
-model.add(Dense(outputs, activation='softmax'))
-opt = rmsprop(lr=0.001, rho =0.95, epsilon=None, decay=0.0)
-model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
-#
-verbose = 1
-epochs = 20
-batch_size = 32
-model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size, verbose=verbose)
-loss, accuracy = model.evaluate(x_test, y_test, batch_size=batch_size, verbose=verbose)
-print('accuracy', accuracy)
-print('loss', loss)
-model.save(path+'/model.h5')
+def run_cnn(x_train,y_train,x_test,y_test,prev_accuracy):
+	# get data dimensions
+	samples = x_train.shape[1]
+	features = x_train.shape[2]
+	outputs = y_train.shape[1]
+	# create cnn model
+	model = Sequential()
+	model.add(Conv1D(filters=64, kernel_size=8, activation='relu', dilation_rate=1, input_shape=(samples,features)))
+	model.add(Dropout(0.5))
+	model.add(Conv1D(filters=64, kernel_size=3, activation='relu', dilation_rate=2))
+	model.add(Dropout(0.5))
+	model.add(MaxPooling1D(pool_size=2))
+	model.add(Conv1D(filters=64, kernel_size=3, activation='relu', dilation_rate=4))
+	model.add(Dropout(0.5))
+	model.add(MaxPooling1D(pool_size=2))
+	model.add(Conv1D(filters=64, kernel_size=3, activation='relu', dilation_rate=8))
+	model.add(Flatten())
+	model.add(Dense(100, activation='relu'))
+	model.add(Dense(outputs, activation='softmax'))
+	opt = rmsprop(lr=0.001, rho =0.99, epsilon=None, decay=0.0)
+	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+	#
+	verbose = 1
+	epochs = 20
+	batch_size = 32
+	model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size, verbose=verbose)
+	loss, accuracy = model.evaluate(x_test, y_test, batch_size=batch_size, verbose=verbose)
+	print('accuracy', accuracy)
+	print('loss', loss)
+	if accuracy > prev_accuracy:
+		model.save(path+'/model.h5')
+	return accuracy
+prev_accuracy = 0
+accuracy = []
+for i in range(10):
+	prev_accuracy = run_cnn(x_train,y_train,x_test,y_test,prev_accuracy)
+	accuracy.append(prev_accuracy)
+print('accuracy',accuracy)
+print('mean accuracy',np.mean(accuracy))
+print('stdev accuracy',np.std(accuracy))
