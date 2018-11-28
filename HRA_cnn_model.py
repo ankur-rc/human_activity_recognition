a8d9cdf8688fb1131889016a26005f36c1ac0f43
jcroc32 <33503909+jcroc32@users.noreply.github.com>
2018-11-28 02:34:18 -0600

    Add files via upload
    deeper wavenet arch with 92% acc

--- HRA_cnn_model.py
+++ HRA_cnn_model.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 import pandas as pd
 from keras.models import Sequential
-from keras.layers import Dense, Flatten, Dropout
+from keras.layers import Dense, Flatten, Dropout, BatchNormalization
 from keras.layers.convolutional import Conv1D, MaxPooling1D
 from keras.optimizers import rmsprop
 from keras.utils import to_categorical
@@ -35,37 +35,34 @@ x_train = np.dstack(x_train)
 y_test = to_categorical(y_test-1)
 y_train = to_categorical(y_train-1)
 def run_cnn(x_train,y_train,x_test,y_test,prev_accuracy):
-	# get data dimensions
-	samples = x_train.shape[1]
-	features = x_train.shape[2]
-	outputs = y_train.shape[1]
-	# create cnn model
-	model = Sequential()
-	model.add(Conv1D(filters=64, kernel_size=8, activation='relu', dilation_rate=1, input_shape=(samples,features)))
-	model.add(Dropout(0.5))
-	model.add(Conv1D(filters=64, kernel_size=3, activation='relu', dilation_rate=2))
-	model.add(Dropout(0.5))
-	model.add(MaxPooling1D(pool_size=2))
-	model.add(Conv1D(filters=64, kernel_size=3, activation='relu', dilation_rate=4))
-	model.add(Dropout(0.5))
-	model.add(MaxPooling1D(pool_size=2))
-	model.add(Conv1D(filters=64, kernel_size=3, activation='relu', dilation_rate=8))
-	model.add(Flatten())
-	model.add(Dense(100, activation='relu'))
-	model.add(Dense(outputs, activation='softmax'))
-	opt = rmsprop(lr=0.001, rho =0.99, epsilon=None, decay=0.0)
-	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-	#
-	verbose = 1
-	epochs = 20
-	batch_size = 32
-	model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size, verbose=verbose)
-	loss, accuracy = model.evaluate(x_test, y_test, batch_size=batch_size, verbose=verbose)
-	print('accuracy', accuracy)
-	print('loss', loss)
-	if accuracy > prev_accuracy:
-		model.save(path+'/model.h5')
-	return accuracy
+  # get data dimensions
+  samples = x_train.shape[1]
+  features = x_train.shape[2]
+  outputs = y_train.shape[1]
+  # create cnn model
+  model = Sequential()
+  model.add(Conv1D(filters=32, kernel_size=5, activation='relu', dilation_rate=1, input_shape=(samples,features)))
+  model.add(BatchNormalization())
+  model.add(Conv1D(filters=32, kernel_size=5, activation='relu', dilation_rate=2))
+  model.add(BatchNormalization())
+  model.add(Conv1D(filters=32, kernel_size=5, activation='relu', dilation_rate=4))
+  model.add(Conv1D(filters=32, kernel_size=5, activation='relu', dilation_rate=8))
+  model.add(MaxPooling1D(pool_size=2))
+  model.add(Dropout(0.5))
+  model.add(Flatten())
+  model.add(Dense(100, activation='relu'))
+  model.add(Dense(outputs, activation='softmax'))
+  model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+  #
+  verbose = 0
+  epochs = 10
+  batch_size = 32
+  model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size, verbose=verbose)
+  loss, accuracy = model.evaluate(x_test, y_test, batch_size=batch_size, verbose=verbose)
+  print('accuracy', accuracy)
+  if accuracy > prev_accuracy:
+    model.save(path+'/model.h5')
+  return accuracy
 prev_accuracy = 0
 accuracy = []
 for i in range(10):
