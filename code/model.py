18040ca241227eda8925f5f6eabccb1bb5e80a60
Ankur Roy Chowdhury <ankurrc@tamu.edu>
2018-11-27 21:38:25 -0600

    Added model class interfaces.

--- code/model.py
+++ code/model.py
@@ -3,3 +3,20 @@ Created Date: Tuesday November 27th 2018
 Last Modified: Tuesday November 27th 2018 9:23:41 pm
 Author: ankurrc
 '''
+
+
+class Model(object):
+
+    def __init__(self, train_data=None, test_data=None):
+        self.verbose = 1
+        self.epochs = 15
+        self.batch_size = 1
+        self.n_timesteps = train_data.X.shape[1]
+        self.n_features = train_data.X.shape[2]
+        self.n_outputs = train_data.y.shape[1]
+
+    def build(self):
+        raise NotImplementedError("Use a subclass!")
+
+    def evaluate(self):
+        raise NotImplementedError("Use a subclass!")
